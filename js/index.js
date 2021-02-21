245a3e397436f3fe1355158ec6deff46f37ffe47
tkdals1049 <37647297+tkdals1049@users.noreply.github.com>
2021-02-22 07:06:28 +0900

    1

--- js/index.js
+++ js/index.js
@@ -217,11 +217,13 @@ function(data, status){
 	if(status!=="success") {
 		swal({
 				title: "ERROR!!!",
-				text: "Please Reset."
+				text: "Please Reset your website."
 			})
 	}
 	profile = data;
 	var pInfo = profile.personalInfo;
+	var profileText='Name: '+pInfo.fname+pInfo.lname+profile.summary;
+	profiletext += 'Hobby: 게임 플레이 및 분석 </br>'+'Phone: '+pInfo.mob+'</br>'+'E-Mail: '+pInfo.email;
 	$('title').html(pInfo.nick+' | Portfolio');
 	$('#blogname').html(pInfo.bname);
 	$('#image img').attr('src','img/'+pInfo.myimg);
@@ -235,12 +237,12 @@ function(data, status){
 		<li class="tab col s3"><a href="#experience">Experience</a></li>
 		<li class="tab col s3"><a href="#education">Education</a></li>
 	`);
-	$('#believe').html('<h4>I 우호</h4><span></span>');
+	$('#believe').html('<h4>Test 01</h4><span></span>');
 	const typed = new Typed('#believe span', {
-		strings: profile.qoutes,
+		strings:profileText,// profile.qoutes,
 		typeSpeed: 40,
 		cursorChar:"_",
-		loop:true
+		loop:false
 	});
 	loadLikes(profile.likes);
 	$('#helloText').html(profile.helloText);
