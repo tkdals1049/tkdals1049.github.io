dfbbd8bb4c193af9aa72488a5c5c8f4850103090
tkdals1049 <tkdals1049@naver.com>
2021-02-22 23:19:39 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -204,7 +204,7 @@ function(data, status){
 	$('#contact').html('Hobby: 게임 플레이 및 분석 </br>'+'Phone: '+pInfo.mob+'</br>'+'E-Mail: '+pInfo.email);
 	$('#summary').html('<span class="center-align"></span>');
 	var tes= ["Name: "+pInfo.fname+pInfo.lname+"</br> Hobby: 게임 플레이 및 분석 </br> Phone: "
-	+pInfo.mob+"</br> E-Mail: "+pInfo.email+profile.summary2
+	+pInfo.mob+"</br> E-Mail: "+pInfo.email+"</br></br>"+profile.summary2
 	  ];
 	  const typed2 = new Typed('#summary span', {
 		strings: tes,
