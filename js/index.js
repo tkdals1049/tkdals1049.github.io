29dfbb51e09397df68c4f09a581b7a4377c20f76
tkdals1049 <tkdals1049@naver.com>
2021-02-23 04:03:40 +0900

    test

--- js/index.js
+++ js/index.js
@@ -203,7 +203,7 @@ function(data, status){
 	$('#name').html('Name: '+pInfo.fname+pInfo.lname);
 	$('#contact').html('</br>Name: '+pInfo.fname+pInfo.lname+'</br>Hobby: 게임 플레이 및 분석 </br>'+'Phone: '+pInfo.mob+'</br>'+'E-Mail: '+pInfo.email);
 	$('#summary').html('<span></span>');
-	var tes= ["<p>Introduction:<br/>"+profile.summary2+"</p>"];
+	var tes= ["Introduction:<br/>"+profile.summary2];
 	  const typed2 = new Typed('#summary span', {
 		strings: tes,
 		typeSpeed: 20,
