9148f58ac239bf956fa22636aabfdc4e37b52927
tkdals1049 <tkdals1049@naver.com>
2021-02-22 23:48:53 +0900

    test

--- js/index.js
+++ js/index.js
@@ -204,8 +204,8 @@ function(data, status){
 	$('#name').html('Name: '+pInfo.fname+pInfo.lname);
 	$('#contact').html('Hobby: 게임 플레이 및 분석 </br>'+'Phone: '+pInfo.mob+'</br>'+'E-Mail: '+pInfo.email);
 	$('#summary').html('<span class="center-align"></span>');
-	var tes= ["<summary>Name: "+pInfo.fname+pInfo.lname+"</br> Hobby: 게임 플레이 및 분석 </br> Phone: "
-	+pInfo.mob+"</br> E-Mail: "+pInfo.email+"</br></br>Introduction:<br/>"+profile.summary2+"</summary>"];
+	var tes= ["Name: "+pInfo.fname+pInfo.lname+"</br> Hobby: 게임 플레이 및 분석 </br> Phone: "
+	+pInfo.mob+"</br> E-Mail: "+pInfo.email+"</br></br>Introduction:<br/>"+profile.summary2];
 	  const typed2 = new Typed('#summary span', {
 		strings: tes,
 		typeSpeed: 40,
