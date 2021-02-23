db611456fadb9623d7d8e7b3bcf5822a76fa0cf1
tkdals1049 <tkdals1049@naver.com>
2021-02-24 07:24:57 +0900

    test

--- js/index.js
+++ js/index.js
@@ -184,6 +184,11 @@ const SaysInnerHtml = `<div class="col m6">
 $('#Says').html(SaysInnerHtml);
 }
 
+swal({
+	title: "Hello World!!!",
+	text: "01번째 실험."
+});
+
 $.get("js/profile.json", 
 function(data, status){
 	console.log('Got profile:',data,' \nwith status:',status);
