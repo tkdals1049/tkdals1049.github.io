852f0d06826134dfa7455106aaaa0a27db7eabcf
tkdals1049 <tkdals1049@naver.com>
2021-02-24 08:17:51 +0900

    test

--- js/index.js
+++ js/index.js
@@ -133,7 +133,7 @@ onWindowResize();
 
 function onWindowResize(){
 	const heightPageA = parseInt($('#pagea').css('height').replace('px',''),100);
-	const tabContentHeight = Math.max(heightPageA-48,(window.innerHeight - 50)) + 'px';
+	const tabContentHeight = '500px';//Math.max(heightPageA-48,(window.innerHeight - 50)) + 'px';
 	console.log(`${document.getElementsByClassName('tabs-content carousel initialized')[0].style.height } to ${tabContentHeight}`);
 	const tabs = document.getElementsByClassName('tabs-content carousel initialized');
 	if (tabs && tabs[0]) {
