697c219b3b68750bbefd56f59e1e3895d3780b6c
tkdals1049 <tkdals1049@naver.com>
2021-02-25 07:27:19 +0900

    test

--- js/index.js
+++ js/index.js
@@ -220,7 +220,7 @@ function(data, status){
 	// });
 	// loadLikes(profile.likes);
 	//loadPics(profile.pics);
-	$('#pics').html('<h4>My Pic</h4><object type="image/svg+xml" data="img/'+profile.likes[1].icon+'">'+profile.likes[1].name+'</object>');
+	$('#pics').html('<h4>My Pic</h4><object type="image/jpeg" data="img/'+profile.pics[1].icon+'">'+profile.pics[1].name+'</object>');
 	//$('#pics').html('<h4>My Pic</h4><div class="row"><div class="col s2"><img src="img/'+profile.profileLinks[0].icon+'"></div></div>');
 	
 	$('#helloText').html(profile.helloText);
